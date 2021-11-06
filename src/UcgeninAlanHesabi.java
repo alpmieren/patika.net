c19fe5143a49687fe73d300a5a3c49e3c59172ac
alperen <alpmieren@gmail.com>
2021-11-06 20:06:03 +0300

    ucgen-alan-hesaplama

---
+++ src/UcgeninAlanHesabi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class UcgeninAlanHesabi {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lutfen ucgenin kenar uzunluklarini giriniz..");
+        double a=scan.nextDouble();
+        double b= scan.nextDouble();
+        double c= scan.nextDouble();
+
+        double u=(a+b+c) / 2;
+        double ucgeninAlanKaresi=u *(u-a)* (u-b)*(u-c);
+        double ucgenAlan=Math.pow(ucgeninAlanKaresi,0.5);
+        System.out.println("ucgenin alani= "+ ucgenAlan);
+
+    }
+}
