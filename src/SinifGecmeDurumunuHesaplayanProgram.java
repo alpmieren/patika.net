dd79cd08d8b054f47b7adb864a690e09c4633f8e
alperen <alpmieren@gmail.com>
2021-11-07 03:37:07 +0300

    Sinif-Gecme-Durumunu-Hesaplayan-Program

---
+++ src/SinifGecmeDurumunuHesaplayanProgram.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SinifGecmeDurumunuHesaplayanProgram {
+    public static void main(String[] args) {
+        int matematik,fizik,turkce,kimya,muzik,sonuc;
+        Scanner scan=new Scanner(System.in);
+        System.out.print("matematik notunuzu giriniz: ");
+        matematik= scan.nextInt();
+        System.out.print("fizik notunuzu giriniz: ");
+        fizik= scan.nextInt();
+        System.out.print("turkce notunuzu giriniz: ");
+        turkce=scan.nextInt();
+        System.out.print("kimya notunuzu giriniz: ");
+        kimya= scan.nextInt();
+        System.out.print("muzik notunuzu giriniz: ");
+        muzik= scan.nextInt();
+        sonuc=(matematik+fizik+turkce+kimya+turkce) / 5;
+
+        if ((matematik>=0 && matematik<=100) && (turkce>=0 || turkce<=100) && (fizik>=0 && fizik<=100) &&
+        (kimya>=0 && kimya<=100) && (muzik>=0 && muzik<=100)) {
+            if (sonuc>=55){
+                System.out.println("Geçti");
+            }else {
+                System.out.println("Kaldı");
+            }
+        }else {
+            System.out.println("Lutfen gecerli notlar giriniz");
+        }
+
+    }
+}
