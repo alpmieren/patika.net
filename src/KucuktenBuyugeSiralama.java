b894a2666e5ef195760002db24a526848bfe1fab
alperen <alpmieren@gmail.com>
2021-11-08 22:14:37 +0300

    kücükten-büyüge-siralama

---
+++ src/KucuktenBuyugeSiralama.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class KucuktenBuyugeSiralama {
+    public static void main(String[] args) {
+
+        int a,b,c;
+        Scanner scan=new Scanner(System.in);
+        System.out.print("birinci sayiyi giriniz: ");
+        a= scan.nextInt();
+        System.out.print("ikinci sayıyı girin: ");
+        b= scan.nextInt();
+        System.out.print("üçüncü sayıyı girin: ");
+        c= scan.nextInt();
+
+        if (a>b && a>c){
+            if (b>c){
+                System.out.println(a +">"+b+">"+c);
+            }else {
+                System.out.println(a+">"+c+">"+b);
+            }
+        }else if (b>a && b>c){
+            if (a>c){
+                System.out.println(b+">"+a+">"+c);
+            }else {
+                System.out.println(b+">"+c+">"+a);
+            }
+        }else {
+            if (a>b){
+                System.out.println(c+">"+a+">"+b);
+            }else {
+                System.out.println(c+">"+b+">"+a);
+            }
+        }
+    }
+}
